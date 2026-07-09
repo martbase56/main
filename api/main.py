86067e530da923e5fc823b921ebd8d423bafd358
martbase56 <marketbasebd.com@gmail.com>
2026-07-09 18:07:38 +0600

    Update main.py

--- api/main.py
+++ api/main.py
@@ -24,6 +24,7 @@ app.include_router(products.router, prefix="/api/products", tags=["Products"])
 app.include_router(orders.router, prefix="/api/orders", tags=["Orders"])
 app.include_router(admin.router, prefix="/api/admin", tags=["Admin Panel"])
 app.include_router(gmail.router, prefix="/api/gmail", tags=["Gmail Tasks"])
+app.include_router(instagram.router, prefix="/api/instagram", tags=["Instagram Tasks"])
 # এপিআই রুট চেক করার জন্য বেসিক টেস্ট এন্ডপয়েন্ট
 @app.get("/api")
 def read_api_root():
